09376d447a3afd1633ec1bbee67d0e47a21d7a56
Martin M Sheeks <martin.sheeks@gmail.com>
2015-02-13 13:35:13 -0600

    replace konamiClass
    Add in code from Daniel Bernard to significantly reduce size of plugin

--- jquery.konami.js
+++ jquery.konami.js
@@ -1,132 +1,41 @@
 (function ( $ ) {
 
-	var konamiClass = {
+	var konamiCute = {
+	    vk_up: "38",
+	    vk_down: "40",
+	    vk_left: "37",
+	    vk_right: "39",
+	    vk_a: "65",
+	    vk_b: "66",
+	    keys: [],
+	    konami: "",
 
-		success: 9,  
-	    last: 0,
-	    step: 0,
-	    pressed: 0,
-	    vk_up: 38,
-	    vk_down: 40,
-	    vk_left: 37,
-	    vk_right: 39,
-	    vk_a: 65,
-	    vk_b: 66,
+	    init: function(opts) {
+	    	if(opts) {
+	    		for (var name in opts) { konamiCute[name] = opts[name]}
+	    	}
+	    	konamiCute.konami = konamiCute.vk_up + "," + konamiCute.vk_up + "," + konamiCute.vk_down + "," + konamiCute.vk_down + "," + konamiCute.vk_left + "," + konamiCute.vk_right + "," + konamiCute.vk_left + "," + konamiCute.vk_right + "," + konamiCute.vk_b + "," + konamiCute.vk_a;
 
-		init: function(opts) {
-			if(opts) {
-				konamiClass.onSuccess = opts.onSuccess;
-			}
-	    	$(document).on("keyup", function (e) {
-			    konamiClass.detect(e.which);
-			});
-	    },
-	    
-	    detect: function(k) {
-	    	konamiClass.pressed = k;
-	        if(k == konamiClass.vk_up) {
-	        	konamiClass.onVkUp(k);
-	        }
-	        if(k == konamiClass.vk_down) {
-	        	konamiClass.onVkDown(k);
-	        }
-	        if(k == konamiClass.vk_left) {
-	        	konamiClass.onVkLeft(k);
-	        }
-	        if(k == konamiClass.vk_right) {
-	        	konamiClass.onVkRight(k);
-	        }
-	        if(k == konamiClass.vk_b) {
-	        	konamiClass.onVkB(k);
-	        }
-	        if(k == konamiClass.vk_a) {
-	        	konamiClass.onVkA(k);
-	        }
-	    },
-
-		onVkUp: function(k) {
-            if(konamiClass.step == 0) {
-                konamiClass.good(k);
-            }
-            else if(konamiClass.step > 1) {
-                konamiClass.reset();
-            }
-            else {
-                if(konamiClass.last == konamiClass.vk_up) {
-                    konamiClass.good(k)
-                }
-            }
-		},
-
-		onVkDown: function(k){
-			if(konamiClass.step < 2) {
-				konamiClass.reset();
-			}
-			else if(konamiClass.step == 2 || konamiClass.step == 3) {
-				konamiClass.good(k);
-			} else {
-				konamiClass.reset();
-			}
-		},
-
-		onVkLeft: function(k) {
-			if(konamiClass.step == 4 || konamiClass.step == 6) {
-				konamiClass.good(k);
-			} else {
-				konamiClass.reset();
-			}
-		},
-
-		onVkRight: function(k) {
-			if(konamiClass.step == 5 || konamiClass.step == 7) {
-				konamiClass.good(k);
-			} else {
-				konamiClass.reset();
-			}
-		},
-
-		onVkA: function(k) {
-			if(konamiClass.step == 9) {
-				konamiClass.good(k);
-			} else {
-				konamiClass.reset();
-			}
-		},
-
-		onVkB: function(k) {
-			if(konamiClass.step == 8) {
-				konamiClass.good(k);
-			} else {
-				konamiClass.reset();
-			}
-		},
-	    
-	    reset: function() {
-	        konamiClass.step = 0;
-	        konamiClass.pressed = 0;
-	        konamiClass.last = 0;
-	    },
-	        
-	    good: function() {
-	        if(konamiClass.step == konamiClass.success) {
-	            konamiClass.onSuccess();
-	            konamiClass.reset();
-	        }
-	        konamiClass.last = konamiClass.pressed;
-	        konamiClass.step++;
-	        return;
+	    	this.on("keydown", function(e){
+		        konamiCute.keys.push(e.which);
+		        if (konamiCute.keys.toString().indexOf(konamiCute.konami) >= 0) {
+		            konamiCute.onSuccess();
+		            konamiCute.keys = [];
+		        };
+		    });
 	    },
 
 	    onSuccess: function() {
-	    	alert('Konami! Yay!');
+	    	alert('yay');
 	    }
-	};
+	    
+	}
 	
 	$.fn.konami = function(options) {
 		if(!options) {
-			return konamiClass.init.apply(this);
+			return konamiCute.init.apply(this);
 		} else if ( typeof options === 'object' ) {
-			return konamiClass.init(options);
+			return konamiCute.init.apply(this,options);
 		} else {
 			return false;
 		}
