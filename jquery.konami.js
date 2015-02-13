b26cf9f6acd7b8289e598dd29f0506c43ea86d7d
Martin M Sheeks <martin.sheeks@gmail.com>
2015-02-13 14:00:06 -0600

    make her pretty

--- jquery.konami.js
+++ jquery.konami.js
@@ -1,24 +1,39 @@
 (function ( $ ) {
 
 	var konamiCute = {
-	    vk_up: "38",
-	    vk_down: "40",
-	    vk_left: "37",
-	    vk_right: "39",
-	    vk_a: "65",
-	    vk_b: "66",
-	    keys: [],
-	    konami: "",
+	    vk_up:     "38",
+	    vk_down:   "40",
+	    vk_left:   "37",
+	    vk_right:  "39",
+	    vk_a:      "65",
+	    vk_b:      "66",
+	    keys:      [],
+	    konami:    "",
 
 	    init: function(opts) {
+	    	//set our options if we were given any
 	    	if(opts) {
-	    		for (var name in opts) { konamiCute[name] = opts[name]}
+	    		for (var name in opts) { 
+	    			konamiCute[name] = opts[name]
+	    		}
 	    	}
-	    	konamiCute.konami = konamiCute.vk_up + "," + konamiCute.vk_up + "," + konamiCute.vk_down + "," + konamiCute.vk_down + "," + konamiCute.vk_left + "," + konamiCute.vk_right + "," + konamiCute.vk_left + "," + konamiCute.vk_right + "," + konamiCute.vk_b + "," + konamiCute.vk_a;
 
-	    	this.on("keydown", function(e){
+	    	//setup the konami pattern
+	    	konamiCute.konami = konamiCute.vk_up 
+	    		  	  + "," + konamiCute.vk_up 
+	    			  + "," + konamiCute.vk_down 
+	    			  + "," + konamiCute.vk_down 
+	    			  + "," + konamiCute.vk_left 
+	    			  + "," + konamiCute.vk_right 
+	    			  + "," + konamiCute.vk_left 
+	    			  + "," + konamiCute.vk_right 
+	    			  + "," + konamiCute.vk_b 
+	    			  + "," + konamiCute.vk_a;
+
+	    	//set a 
+	    	$(document).on("keydown", function(e){
 		        konamiCute.keys.push(e.which);
-		        if (konamiCute.keys.toString().indexOf(konamiCute.konami) >= 0) {
+		        if ( konamiCute.keys.toString().indexOf(konamiCute.konami) >= 0 ) {
 		            konamiCute.onSuccess();
 		            konamiCute.keys = [];
 		        };
@@ -28,8 +43,7 @@
 	    onSuccess: function() {
 	    	alert('yay');
 	    }
-	    
-	}
+	};
 	
 	$.fn.konami = function(options) {
 		if(!options) {
